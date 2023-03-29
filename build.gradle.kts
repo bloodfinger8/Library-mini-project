2832ad29bf0093ee04483c41fdc611080ba4f7c5
bloodfinger <didwodn8822@gmail.com>
2023-03-29 21:12:21 +0900

    FIX -  gradle 파일 변경 및 lock 테스트 코드 작성

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.6.8"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
+	kotlin("kapt") version "1.6.21"
+}
+
+group = "com.group"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+	implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+	runtimeOnly("com.h2database:h2")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
+
+	//yml
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+	//QueryDSL
+	implementation("com.querydsl:querydsl-jpa:5.0.0")
+	kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
+
+	//swagger
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+
+	//jjwt
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
--- src/test/kotlin/com/group/libraryapp/service/book/BookConcurrencyTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookConcurrencyTest.kt
@@ -1,15 +1,52 @@
 package com.group.libraryapp.service.book
 
+import com.group.libraryapp.domain.book.Book
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.book.type.BookType
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
-import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
+import com.group.libraryapp.dto.book.request.BookLoanRequest
+import com.group.libraryapp.exception.NotExistStock
+import com.group.libraryapp.security.AuthenticationDTO
+import com.group.libraryapp.usecase.book.BookService
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.ApplicationContext
+import org.springframework.dao.OptimisticLockingFailureException
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Executors
 
 @SpringBootTest
 class BookConcurrencyTest @Autowired constructor(
-    private val userRepository: UserRepository,
-): BehaviorSpec({
-})
+    val bookService: BookService,
+    val bookRepository: BookRepository,
+    val userRepository: UserRepository,
+) {
+
+    @Test
+    fun `도서 선점 대출시 예외 발생 테스트`() {
+        val book = bookRepository.save(Book.create("클린 아키텍처"))
+        val user = userRepository.save(User(BookServiceTest.EMAIL, BookServiceTest.PASSWORD, BookServiceTest.NAME))
+        val executorService = Executors.newFixedThreadPool(3);
+
+        val bookLoanRequest = BookLoanRequest(book.id!!)
+        val auth = AuthenticationDTO.of(user.email.email!!, user.name)
+
+        val future  = executorService.submit { bookService.loan(bookLoanRequest, auth) }
+        val future2 = executorService.submit { bookService.loan(bookLoanRequest, auth) }
+        val future3 = executorService.submit { bookService.loan(bookLoanRequest, auth) }
+
+        var result = Exception()
+        try {
+            future.get()
+            future2.get()
+            future3.get()
+        } catch (e: ExecutionException){
+            result = e.cause as Exception
+        }
+
+        Assertions.assertTrue(result is OptimisticLockingFailureException);
+    }
+}
