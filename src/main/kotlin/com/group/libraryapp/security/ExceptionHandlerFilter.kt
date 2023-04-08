74f68f107b9dcd4e20820ab39530e7d2d94a18f0
bloodfinger <didwodn8822@gmail.com>
2023-04-08 14:20:23 +0900

    FIX - 토큰값 예외 코드 변경

--- src/main/kotlin/com/group/libraryapp/security/ExceptionHandlerFilter.kt
+++ src/main/kotlin/com/group/libraryapp/security/ExceptionHandlerFilter.kt
@@ -2,6 +2,7 @@ package com.group.libraryapp.security
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.group.libraryapp.dto.response.FailureRes
+import com.group.libraryapp.exception.UNAUTHORIZED
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.JwtException
 import org.springframework.http.HttpStatus
@@ -24,17 +25,17 @@ class ExceptionHandlerFilter: OncePerRequestFilter() {
             filterChain.doFilter(request, response);
         } catch (e: JwtException) {
             when(e) {
-                is ExpiredJwtException -> response(50001,"JWT Token has expired", response)
-                else -> response(50002, "JWT token valid error" ,response)
+                is ExpiredJwtException -> response("JWT Token has expired", response)
+                else -> response("JWT token valid error" ,response)
             }
         }
     }
 
-    private fun response(status: Int, message: String, response: HttpServletResponse) {
+    private fun response(message: String, response: HttpServletResponse) {
         response.status = HttpStatus.INTERNAL_SERVER_ERROR.value()
         response.contentType = MediaType.APPLICATION_JSON_VALUE
         try {
-            response.writer.write(ObjectMapper().writeValueAsString(FailureRes(status,message)));
+            response.writer.write(ObjectMapper().writeValueAsString(FailureRes(UNAUTHORIZED,message)));
         } catch (e: IOException) {
             e.printStackTrace()
         }
